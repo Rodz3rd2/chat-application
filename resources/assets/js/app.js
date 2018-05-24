c812e97167d0ea3a18e21d0f4e7961dd5c56ec75
rodz3rd2 <rodrigogalura3rd@gmail.com>
2018-05-24 16:44:14 +0800

    append message after enter

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,15 +1,45 @@
 var Chat = {
+    ENTER: 13,
+
     init: function() {
         Chat.scrollDown();
 
-        $("#addClass").click(function() {
-            Chat.scrollDown();
-            $('#qnimate').addClass('popup-box-on');
-        });
+        $("#addClass").click(Chat.open);
+        $("#removeClass").click(Chat.close);
+        $('#status_message').keyup(Chat.typing);
+    },
 
-        $("#removeClass").click(function() {
-            $('#qnimate').removeClass('popup-box-on');
-        });
+    open: function() {
+        Chat.scrollDown();
+        $('#qnimate').addClass('popup-box-on');
+    },
+
+    close: function() {
+        $('#qnimate').removeClass('popup-box-on');
+    },
+
+    typing: function(e) {
+        var key_code = e.which;
+
+        if (key_code === Chat.ENTER) {
+            Chat.enter();
+        }
+    },
+
+    enter: function() {
+        var template = _.template($('#message-tmpl').html());
+
+        $('.direct-chat-messages').append(template({
+            message_date: "October 8th, 2015",
+            chat_name: "Java Man",
+            image: "/img/me.jpg",
+            message: $('#status_message').val(),
+            message_time: "3.36 PM",
+            chat_lastname: "Ahoo ahoo"
+        }));
+
+        $('#status_message').val("");
+        Chat.scrollDown();
     },
 
     scrollDown: function() {
